23d438cb06fe6bbf48489acccd4dba814c12e884
GabrielC67 <gabecruzpro@gmail.com>
2025-03-12 21:49:20 -0400

    Checkpoint

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -2,6 +2,7 @@ package io.zipcoder;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.apache.maven.shared.utils.StringUtils.split;
 
@@ -15,46 +16,44 @@ public class StringsAndThings {
      * but not the 'y' in "yellow" (not case sensitive). We'll say that a y or z is at the end of a word if there is not an alphabetic
      * letter immediately following it. (Note: Character.isLetter(char) tests if a char is an alphabetic letter.)
      * example : countYZ("fez day"); // Should return 2
-     *           countYZ("day fez"); // Should return 2
-     *           countYZ("day fyyyz"); // Should return 2
+     * countYZ("day fez"); // Should return 2
+     * countYZ("day fyyyz"); // Should return 2
      */
-    public Integer countYZ(String input){
+    public Integer countYZ(String input) {
+        int y = 0;
 //        String input = "fez day";
 //        Integer expected = 2;
 //        Integer actual = stringsAndThings.countYZ(input);
 //        Assert.assertEquals(expected, actual);
-        char[] inpChar = input.toCharArray();
-        char targetIsy = 'y'; //I seperated these because I want this code to be readable.
-        char targetIsz = 'z';
-        int counterY = 0; //Again.. Readability
-        int counterZ = 0;
-        int totalCounter = 0; //You know it.. Readability lol.
-        for(int i = 0; i < inpChar.length; i++){
-            if((inpChar[i] == targetIsy)) {
-                counterY++;
-            } else if ((inpChar[i] == targetIsz)){
-                counterZ++;
+        String[] inputSplt = input.split(" ");
+        char targetY = 'y', targetZ = 'z';
+        int counter = 0;
+        for(String i : inputSplt){
+            if(i.length() > 0) {
+                char lstChr = i.charAt(i.length() - 1);
+                if (lstChr == targetY || lstChr == targetZ){
+                    counter++;
+                }
             }
-            totalCounter = counterY + counterZ;
         }
+        return counter; //Return Integer 2
 
-        return totalCounter;
     }
+
+
 
     /**
      * Given two strings, base and remove, return a version of the base string where all instances of the remove string have
      * been removed (not case sensitive). You may assume that the remove string is length 1 or more.
      * Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
-     *
+     * <p>
      * example : removeString("Hello there", "llo") // Should return "He there"
-     *           removeString("Hello there", "e") //  Should return "Hllo thr"
-     *           removeString("Hello there", "x") // Should return "Hello there"
+     * removeString("Hello there", "e") //  Should return "Hllo thr"
+     * removeString("Hello there", "x") // Should return "Hello there"
      */
-    public String removeString(String base, String remove){
+    public String removeString(String base, String remove) {
         //        String input = "day fez";
         //        Integer expected = 2;
-
-
 
 
         return null;
@@ -63,12 +62,12 @@ public class StringsAndThings {
     /**
      * Given a string, return true if the number of appearances of "is" anywhere in the string is equal
      * to the number of appearances of "not" anywhere in the string (case sensitive)
-     *
+     * <p>
      * example : containsEqualNumberOfIsAndNot("This is not")  // Should return false
-     *           containsEqualNumberOfIsAndNot("This is notnot") // Should return true
-     *           containsEqualNumberOfIsAndNot("noisxxnotyynotxisi") // Should return true
+     * containsEqualNumberOfIsAndNot("This is notnot") // Should return true
+     * containsEqualNumberOfIsAndNot("noisxxnotyynotxisi") // Should return true
      */
-    public Boolean containsEqualNumberOfIsAndNot(String input){
+    public Boolean containsEqualNumberOfIsAndNot(String input) {
         return null;
     }
 
@@ -76,10 +75,10 @@ public class StringsAndThings {
      * We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
      * Return true if all the g's in the given string are happy.
      * example : gHappy("xxggxx") // Should return  true
-     *           gHappy("xxgxx") // Should return  false
-     *           gHappy("xxggyygxx") // Should return  false
+     * gHappy("xxgxx") // Should return  false
+     * gHappy("xxggyygxx") // Should return  false
      */
-    public Boolean gIsHappy(String input){
+    public Boolean gIsHappy(String input) {
         return null;
     }
 
@@ -88,10 +87,12 @@ public class StringsAndThings {
      * We'll say that a "triple" in a string is a char appearing three times in a row.
      * Return the number of triples in the given string. The triples may overlap.
      * example :  countTriple("abcXXXabc") // Should return 1
-     *            countTriple("xxxabyyyycd") // Should return 3
-     *            countTriple("a") // Should return 0
+     * countTriple("xxxabyyyycd") // Should return 3
+     * countTriple("a") // Should return 0
      */
-    public Integer countTriple(String input){
+    public Integer countTriple(String input) {
         return null;
     }
 }
+
+
